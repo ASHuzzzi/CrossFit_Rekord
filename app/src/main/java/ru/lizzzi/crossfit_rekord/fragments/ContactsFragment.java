9a680bb934089cb84b46ded40d06623fe1f443de
Aleksandr Shupilo <bason.89@mail.ru>
2018-09-14 22:59:01 +0300

    Доработка экрана контактов. ContactsFragment:  - Добавил кнопку инстаграмма и контакта. Размер кнопок будет подстраиваться под размер экрана. Еще есть проверка на наличия приложения (ВК/Инста). Если приложения нет, то ссылка откроется в браузере.

--- app/src/main/java/ru/lizzzi/crossfit_rekord/fragments/ContactsFragment.java
+++ app/src/main/java/ru/lizzzi/crossfit_rekord/fragments/ContactsFragment.java
@@ -1,13 +1,16 @@
 package ru.lizzzi.crossfit_rekord.fragments;
 
-
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import ru.lizzzi.crossfit_rekord.R;
@@ -15,8 +18,10 @@ import ru.lizzzi.crossfit_rekord.interfaces.InterfaceChangeTitle;
 
 public class ContactsFragment extends Fragment {
 
+    private ImageButton ibInstagram;
+    private ImageButton ibVk;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_contacts, container, false);
 
@@ -29,7 +34,45 @@ public class ContactsFragment extends Fragment {
             e.printStackTrace();
         }
 
+        ibInstagram = v.findViewById(R.id.ibInstagram);
+        ibInstagram.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
 
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+
+                PackageManager pm = getContext().getPackageManager();
+                if (isPackageInstalled("com.instagram.android", pm)){
+                    intent.setPackage("com.instagram.android");
+                }else {
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                }
+
+                intent.setData(Uri.parse(getResources().getString(R.string.Instagram)));
+                startActivity(intent);
+
+            }
+        });
+
+        ibVk = v.findViewById(R.id.ibVK);
+        ibVk.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+
+                PackageManager pm = getContext().getPackageManager();
+                if (isPackageInstalled("com.vkontakte.android", pm)){
+                    intent.setPackage("com.vkontakte.android");
+                }else {
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                }
+
+                intent.setData(Uri.parse(getResources().getString(R.string.groupVK)));
+                startActivity(intent);
+
+            }
+        });
 
         return v;
     }
@@ -42,6 +85,26 @@ public class ContactsFragment extends Fragment {
             listernerChangeTitle.changeTitle(R.string.title_Contacts_Fragment, R.string.title_Contacts_Fragment);
         }
 
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenHeight = displaymetrics.heightPixels;
+        int reSizeScreenHeight = screenHeight / 10;
+
+        ibInstagram.getLayoutParams().height = reSizeScreenHeight;
+        ibInstagram.getLayoutParams().width = reSizeScreenHeight;
+        ibVk.getLayoutParams().height = reSizeScreenHeight;
+        ibVk.getLayoutParams().width = reSizeScreenHeight;
+
+    }
+
+    private boolean isPackageInstalled(String packagename, PackageManager packageManager) {
+        try {
+            packageManager.getPackageInfo(packagename, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
