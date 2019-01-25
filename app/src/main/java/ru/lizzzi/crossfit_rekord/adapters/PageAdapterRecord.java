38c0026d522bb95be7a6a2549de2be88a2732883
Aleksandr Shupilo <bason.89@mail.ru>
2019-01-25 09:34:20 +0300

    Обновление (25/01/19) Ввиду открытия в ближайщее время второго зала, адаптирю приложение под работу двух залов. - Добавл в RecordForTrainingSelectFragment табы. всю разметку и логику загрузки старого расписания вынес в TL1ParnasFragment

---
+++ app/src/main/java/ru/lizzzi/crossfit_rekord/adapters/PageAdapterRecord.java
@@ -0,0 +1,49 @@
+package ru.lizzzi.crossfit_rekord.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ru.lizzzi.crossfit_rekord.R;
+import ru.lizzzi.crossfit_rekord.fragments.RecordForTrainingTL.TL1ParnasFragment;
+import ru.lizzzi.crossfit_rekord.fragments.RecordForTrainingTL.TL2MyzhestvoFragment;
+
+public class PageAdapterRecord extends FragmentPagerAdapter {
+    private Context mContext;
+
+    public PageAdapterRecord(FragmentManager fm, Context context) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new TL1ParnasFragment();
+        } else if (position == 1){
+            return new TL2MyzhestvoFragment();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    // This determines the title for each tab
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.strSelectTlParnas);
+            case 1:
+                return mContext.getString(R.string.strSelectTlMyzhestvo);
+            default:
+                return null;
+        }
+    }
+}
---
+++ app/src/main/java/ru/lizzzi/crossfit_rekord/fragments/RecordForTrainingTL/TL2MyzhestvoFragment.java
@@ -0,0 +1,19 @@
+package ru.lizzzi.crossfit_rekord.fragments.RecordForTrainingTL;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.lizzzi.crossfit_rekord.R;
+
+public class TL2MyzhestvoFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_record_for_training_tl2_myzhestvo, container, false);
+    }
+
+}
