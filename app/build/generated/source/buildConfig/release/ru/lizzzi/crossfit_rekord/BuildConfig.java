be72bb2c7a1386817cff79585b08278d382a8668
Aleksandr Shupilo <bason.89@mail.ru>
2019-09-10 22:14:03 +0300

    Релиз (10/09/19) Версия 3.15.0

---
+++ app/build/generated/source/buildConfig/release/ru/lizzzi/crossfit_rekord/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ru.lizzzi.crossfit_rekord;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ru.lizzzi.crossfit_rekord";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "3.14.0";
+}
